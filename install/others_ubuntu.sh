e7d4dc4a19b366311f7b7da4865319c1abe5f89e
Karthick J <kjeyapal@akamai.com>
2018-10-18 07:43:35 +0530

    others_ubuntu.sh : Added the missing webob python module

--- install/others_ubuntu.sh
+++ install/others_ubuntu.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y v4l-utils libasound-dev yasm
 sudo apt install -y python
 sudo apt install -y python-pip
-sudo pip install webapp2 psutil
+sudo pip install webapp2 webob psutil
 sudo apt install -y apache2 libapache2-mod-wsgi
 sudo adduser www-data video
 sudo adduser www-data audio
