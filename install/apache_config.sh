b4425c6b3a9ceb6755b0008e9bb0f590e3c67bcd
Karthick J <kjeyapal@akamai.com>
2018-11-19 15:42:24 +0530

    apache_config.sh : Added read and execute to the parent folders as required by Apache

--- install/apache_config.sh
+++ install/apache_config.sh
@@ -5,3 +5,5 @@ set -o pipefail
 work_dir=`pwd`
 sed "s|<working directory>|$work_dir|" < install/directory.conf | sudo tee -a $1
 sed "s|<working directory>|$work_dir|" < install/vhost.conf | sudo tee -a $2
+# Add read + execute permissions to all parent directories
+while [[ $work_dir != / ]]; do sudo chmod a+rx "$work_dir"; work_dir=$(dirname "$work_dir"); done;
