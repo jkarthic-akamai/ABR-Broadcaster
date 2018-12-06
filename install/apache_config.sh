09b7d9b9c8a3d27036812efe7e57a1a2e3b61b77
Karthick J <kjeyapal@akamai.com>
2018-12-06 14:22:27 +0530

    apache_config.sh : Avoid repeated addition of same configs

--- install/apache_config.sh
+++ install/apache_config.sh
@@ -3,7 +3,11 @@
 set -e
 set -o pipefail
 work_dir=`pwd`
-sed "s|<working directory>|$work_dir|" < install/directory.conf | sudo tee -a $1
-sed "s|<working directory>|$work_dir|" < install/vhost.conf | sudo tee -a $2
+if ! grep -q $work_dir $1; then
+  sed "s|<working directory>|$work_dir|" < install/directory.conf | sudo tee -a $1
+fi
+if ! grep -q $work_dir $2; then
+  sed "s|<working directory>|$work_dir|" < install/vhost.conf | sudo tee -a $2
+fi
 # Add read + execute permissions to all parent directories
 while [[ $work_dir != / ]]; do sudo chmod a+rx "$work_dir"; work_dir=$(dirname "$work_dir"); done;
